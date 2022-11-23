3ebaadeec06e0111acf2fbd04130f56b4bc9bd13
knightfury16 <suhaibknight@gmail.com>
2022-11-23 12:23:09 +0600

    404 page modified.

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,7 +6,7 @@ const ErrorPage = () => (
       <div className="container">
         <h1>Error 404</h1>
         <p>Woops. Looks like this page doesn't exist</p>
-        <a href="#" className="btn btn--rounded btn--yellow">Go to home</a>
+        <a href="/" className="btn btn--rounded btn--yellow">Go to home</a>
       </div>
     </section>
   </LayoutError>
